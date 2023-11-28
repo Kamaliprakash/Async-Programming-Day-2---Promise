8db7549c72e872a7cf7c311eefe93a3145d4a3f9
Kamaliprakash <kamaliprakash0307@gmail.com>
2023-11-28 10:43:52 +0530

    dom day 2

---
+++ script.js
@@ -0,0 +1,19 @@
+// const a = parseInt( prompt ("Enter a number"))
+
+// const promise1 = new Promise((resolve,reject)=>{
+//     if(a%2===0){
+//         resolve("Even number")
+//     }
+//     else{
+//         reject("odd number")
+//     }
+// })
+// promise1.then((data)=> console.log(data)).catch((error)=>console.log(error))
+
+// session task
+
+
+
+
+
+
